4565344dce405348fd089d44c49199d222c96a14
pablodeges <degespablo@gmail.com>
2023-04-02 00:19:43 +0200

    first com :3

---
+++ cat.py
@@ -0,0 +1,36 @@
+from instagrapi import Client
+import accdata
+import random
+import datetime
+import time
+import os
+import requests
+
+
+cl = Client()
+cl.login(accdata.username, accdata.password)
+meows = ["mow", "meow", "mreoww", "meep", "miau", "mauu"]
+rr = random.randint(0,5)
+
+while True:
+	# make cat api request and download
+	
+	page = requests.get("https://cataas.com/cat")
+
+	f_ext = os.path.splitext("https://cataas.com/cat")[-1]
+	f_name = 'img{}'.format(f_ext)
+	with open(f_name, 'wb') as f:
+		f.write(page.content)
+
+
+	# upload cat image
+	try:
+		cl.photo_upload("/Users/pdeges/Desktop/code/python/igcats/img", meows[rr])
+		print("upload at " + str(datetime.datetime.now()) + " successful")
+	except:
+		print("something went wrong :'3")
+
+
+	# sleep for 6h
+	time.sleep(21600)
+
