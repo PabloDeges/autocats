0924fbf4331c76d58253890d41c63760d853bfe6
pablodeges <degespablo@gmail.com>
2023-04-11 11:01:05 +0200

    fixed indentation - now 4 spaces no tabs

--- cat.py
+++ cat.py
@@ -14,30 +14,31 @@ meows = ["mow", "meow", "mreoww", "meep", "miau", "mauu"]
 rr = random.randint(0,5)
 
 while True:
-	# make cat api request and download
-	
-	page = requests.get("https://cataas.com/cat")
-
-	f_ext = os.path.splitext("https://cataas.com/cat")[-1]
-	f_name = 'cat_img{}'.format(f_ext)
-	with open(f_name, 'wb') as f:
-            f.write(page.content)
-
-
-	# upload cat image
-	try:
-            print("called me")
-            cl.photo_upload("/Users/pdeges/Desktop/code/python/igcats/cat_img", meows[rr])
-            print("upload at " + str(datetime.datetime.now()) + " successful")
-	except Exception as e:
-            print("something went wrong :'3")
-            if str(type(e)) == "<class 'instagrapi.exceptions.UnknownError'>":
-                
-                print(e)
-                continue
-                time.sleep(1000)
-
-
-	# 120 sec buffer
-	time.sleep(120)
-
+    # make cat api request and download
+    
+    page = requests.get("https://cataas.com/cat")
+    
+    f_ext = os.path.splitext("https://cataas.com/cat")[-1]
+    f_name = 'cat_img{}'.format(f_ext)
+    with open(f_name, 'wb') as f:
+        f.write(page.content)
+    
+    
+    # upload cat image
+    try:
+        print("called me")
+        cl.photo_upload("/Users/pdeges/Desktop/code/python/igcats/cat_img", meows[rr])
+        print("upload at " + str(datetime.datetime.now()) + " successful")
+    except Exception as e:
+        print("something went wrong :'3")
+        if str(type(e)) == "<class 'instagrapi.exceptions.UnknownError'>":
+            print(e)
+            time.sleep(1000)
+	        
+	    
+    
+    
+    # 120 sec buffer
+    time.sleep(120)
+    
+    
